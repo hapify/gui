b11449a753a0c886d7e6e43068fe1681017a2800
Edouard Demotes-Mainard <e.demotes@gmail.com>
2019-02-07 10:22:14 -0500

    Remove unused methods

--- src/app/preset/components/root/root.component.ts
+++ src/app/preset/components/root/root.component.ts
@@ -1,11 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { StorageService } from '../../services/storage.service';
 import { IPreset } from '../../interfaces/preset';
-import { MessageService } from '@app/services/message.service';
-import { WebSocketService } from '@app/services/websocket.service';
-import { WebSocketMessages } from '@app/interfaces/websocket-message';
 import { IModel } from '@app/model/interfaces/model';
-import { ModelService } from '@app/preset/services/model.service';
 
 interface PresetMergeResults {
 	created: IModel[];
@@ -21,13 +17,7 @@ export class RootComponent implements OnInit {
 	@Output() presetApplied = new EventEmitter<PresetMergeResults>();
 
 	/** Constructor */
-	constructor(
-		private storageService: StorageService,
-		// private modelStorageService: ModelStorageService,
-		private messageService: MessageService,
-		private webSocketService: WebSocketService,
-		private modelService: ModelService
-	) {}
+	constructor(private storageService: StorageService) {}
 
 	/**
 	 * Preset instances
@@ -41,21 +31,6 @@ export class RootComponent implements OnInit {
 	 */
 	ngOnInit() {
 		this.updatePresets();
-	}
-
-	/**
-	 * Called when the user apply the preset
-	 */
-	async applyPreset(preset: IPreset): Promise<void> {
-		const results = (await this.webSocketService.send(
-			WebSocketMessages.APPLY_PRESETS,
-			{
-				models: preset.models
-			}
-		)) as PresetMergeResults;
-
-		this.presetApplied.emit(results);
-		this.modelService.presetApplied.next(results);
 	}
 
 	/**
