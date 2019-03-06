4d4357b1e5bde191ecc222ee4c47378a42a5d602
Edouard Demotes <e.demotes@gmail.com>
2019-03-06 18:32:03 -0500

    Remove unused compoents and update translations

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,17 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModelModule, MODEL_ROUTES } from './model/model.module';
 import { ModelComponent } from './components/model/model.component';
 
-/*import { PresetModule, PRESET_ROUTES } from './preset/preset.module';
-import { PresetComponent } from './components/preset/preset.component';*/
-
 import { ChannelModule, CHANNEL_ROUTES } from './channel/channel.module';
 import { ChannelComponent } from './components/channel/channel.component';
-
-import {
-	DocumentationModule,
-	DOCUMENTATION_ROUTES
-} from './documentation/documentation.module';
-import { DocumentationComponent } from '@app/components/documentation/documentation.component';
 
 export const routes: Routes = [
 	{
@@ -27,31 +18,15 @@ export const routes: Routes = [
 		component: ModelComponent,
 		children: MODEL_ROUTES
 	},
-	/*{
-		path: 'preset',
-		component: PresetComponent,
-		children: PRESET_ROUTES
-	},*/
 	{
 		path: 'channel',
 		component: ChannelComponent,
 		children: CHANNEL_ROUTES
-	},
-	{
-		path: 'documentation',
-		component: DocumentationComponent,
-		children: DOCUMENTATION_ROUTES
 	}
 ];
 
 @NgModule({
-	imports: [
-		ModelModule,
-		/*PresetModule,*/
-		ChannelModule,
-		DocumentationModule,
-		RouterModule.forRoot(routes)
-	],
+	imports: [ModelModule, ChannelModule, RouterModule.forRoot(routes)],
 	exports: [RouterModule]
 })
 export class AppRoutingModule {}
