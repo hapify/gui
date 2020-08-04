2ab6cdfd61a4d434f13e062386d385fba95e224c
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-08-04 11:26:45 -0400

    refactor: sync folder structure with ng10

--- karma.conf.js
+++ karma.conf.js
@@ -3,24 +3,22 @@
 
 module.exports = function(config) {
 	config.set({
-		basePath: '..',
-		frameworks: ['jasmine', '@angular/cli'],
+    basePath: 'src',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
 		plugins: [
 			require('karma-jasmine'),
 			require('karma-chrome-launcher'),
 			require('karma-jasmine-html-reporter'),
 			require('karma-coverage-istanbul-reporter'),
-			require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma')
 		],
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
 		coverageIstanbulReporter: {
-			reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, './coverage/hapify-gui'),
+      reports: ['html', 'lcovonly', 'text-summary'],
 			fixWebpackSourcePaths: true
-		},
-		angularCli: {
-			environment: 'dev'
 		},
 		reporters: ['progress', 'kjhtml'],
 		port: 9876,
@@ -28,6 +26,7 @@ module.exports = function(config) {
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
 		browsers: ['Chrome'],
-		singleRun: false
+    singleRun: false,
+    restartOnFileChange: true
 	});
 };
