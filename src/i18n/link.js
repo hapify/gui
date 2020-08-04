b782c4e8b311f87cc5af9bcde9029e4ed3d585d9
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-08-04 10:59:55 -0400

    build(i18n): remove i18n script

--- src/i18n/link.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Show the sheet link in the console
-
-const config = require('./config');
-
-console.log(
-	`https://docs.google.com/spreadsheets/d/${config.spreadsheetId}/edit#gid=0`
-);
